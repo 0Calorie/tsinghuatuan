943e13025d97f26cfa4c4259e384427ee1494719
Kira <user@userdeMacBook-Pro-3.local>
2014-11-25 23:33:12 +0800

    ticket handler models again

--- urlhandler/urlhandler/models.py
+++ urlhandler/urlhandler/models.py
@@ -60,7 +60,7 @@ class Ticket(models.Model):
     activity = models.ForeignKey(Activity)
     status = models.IntegerField()
     seat_status = models.IntegerField()
-    seat_id = models.IntegerField()
+    seat = models.ForeignKey(Seat, null=True)
     select_start = models.DateField()
     select_end = models.DateField()
     additional_ticket_id = models.IntegerField()
