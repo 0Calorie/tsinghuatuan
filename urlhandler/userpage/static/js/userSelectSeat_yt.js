e3a9c9260de58610a3c9207f2844b8a350a3570a
wangyuting <wytthuss@126.com>
2014-12-21 23:16:04 +0800

    seat_XQ_scale_test1

--- urlhandler/userpage/static/js/userSelectSeat_yt.js
+++ urlhandler/userpage/static/js/userSelectSeat_yt.js
@@ -246,11 +246,14 @@ function back()
     $(".Information").css("display","none");
     $(".seat").empty();
     $(".seatSelectSeats").empty();
+    $("#result").css("display","none");
+    $(".failure").css("display","none");
 }
 
 function confirm()
 {
-
+    $("#result").css("display","block");
+    $(".failure").css("display","block");
 }
 
 function onclick_seat()
@@ -303,13 +306,13 @@ function addIllustration(word, color)
 {
     var div_illu = document.createElement("div");
     var div_color = document.createElement("div");
-    var div_word = document.createElement("div");
+    var p_word = document.createElement("p");
     $(div_color).addClass("illu_color");
     $(div_color).css("background-color",color);
-    $(div_word).addClass("illu_word");
-    div_word.innerText = word;
+    $(p_word).addClass("illu_word");
+    p_word.innerText = word;
     $(div_illu).addClass("illu_wrap");
-    $(div_illu).append(div_color, div_word);
+    $(div_illu).append(div_color, p_word);
     $(".illustration").append(div_illu);
 }
 
@@ -318,3 +321,28 @@ $(document).ready(function(){
     addIllustration("可选",color_defaultSeat);
     addIllustration("选中",color_selectSeat);
 });
+function scale()
+{
+    var target = document.getElementById("target");
+    target.style.webkitTransition = 'all ease 0.05s';
+
+    touch.on('#target', 'touchstart', function(ev){
+        ev.preventDefault();
+    });
+
+    var initialScale = 1;
+    var currentScale;
+
+    touch.on('#target', 'pinchend', function(ev){
+        currentScale = ev.scale - 1;
+        currentScale = initialScale + currentScale;
+        currentScale = currentScale > 2 ? 2 : currentScale;
+        currentScale = currentScale < 1 ? 1 : currentScale;
+        this.style.webkitTransform = 'scale(' + currentScale + ')';
+        log("当前缩放比例为:" + currentScale + ".");
+    });
+
+    touch.on('#target', 'pinchend', function(ev){
+        initialScale = currentScale;
+    });
+}
