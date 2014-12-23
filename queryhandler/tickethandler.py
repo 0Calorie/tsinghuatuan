12df7bc3087b061f6d0f784eb87bd74a67342e30
wangyuting <wytthuss@126.com>
2014-12-23 21:49:26 +0800

    cancel ticket

--- queryhandler/tickethandler.py
+++ queryhandler/tickethandler.py
@@ -349,7 +349,11 @@ def response_cancel_ticket(msg):
             if tickets.exists():  # user has already booked the activity
                 for ticket in tickets:
                     ticket.status = 0
-                    ticket.seat.status = 0
+                    seat = ticket.seat
+                    print seat
+                    if(seat):
+                        seat.status = 0
+                        seat.save()
                     ticket.save()
                     Activity.objects.filter(id=activity.id).update(remain_tickets=F('remain_tickets') + 1)
                 return get_reply_text_xml(msg, get_text_success_cancel_ticket())
