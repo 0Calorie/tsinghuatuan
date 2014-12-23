96e654a9c87296cdb01f622c7a47ecb18aad5498
Dino Huang <dinokid@yahoo.com>
2014-12-23 23:30:31 +0800

    phone number 5

--- urlhandler/userpage/static/js/userSelectSeat.js
+++ urlhandler/userpage/static/js/userSelectSeat.js
@@ -157,12 +157,11 @@ function showPlace()
 {
     $(".XQ").css("display", "block");
     addIllustration("不可选",color_defaultSeat);
-    addIllustration("可选",color_onSaleSeat);
     addIllustration("选中",color_selectSeat);
-    /*var colorIndex = 0;
+    var colorIndex = 0;
     for(colorIndex;colorIndex<totalPriceLength;colorIndex++){
         addIllustration((" " + totalPrice[colorIndex] + " "), seatPalette[colorIndex]);
-    }*/
+    }
     scale();
     drag();
 }
